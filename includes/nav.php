182c11847e235f7cafea0c2f2d3665881a5f454e
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-03-02 10:33:43 +0100

    modification de la nav

--- includes/nav.php
+++ includes/nav.php
@@ -2,6 +2,21 @@
     <nav role="navigation">
         <ul>
             <li><a href="index.php?page=home">Accueil</a></li>
+            <?php
+                if(isset($_SESSION['login']) && $_SESSION['login'] === true) {
+                    echo "<li><a href=\"index.php?page=logout\">Logout</a></li>";
+                    echo "<li><a href=\"index.php?page=account\">Mon compte</a></li>";
+                }
+                else {
+                    echo "<li><a href=\"index.php?page=login\">Login</a></li>";
+                    echo "<li><a href=\"index.php?page=inscription\">Inscription</a></li>";
+                }
+            ?>
+            <?php
+                if((isset($_SESSION['login']) && $_SESSION['login'] === true) && $_SESSION['role'] >= 2 ) {
+                    echo "<li><a href=\"index.php?page=admin\">Administration</a></li>";
+                }
+            ?>
         </ul>
     </nav>
 </header>
