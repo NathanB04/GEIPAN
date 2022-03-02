de0144ec2a1f93d5552b02a432a39b4477a16218
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-03-02 16:52:40 +0100

    ajout minutes durée évènement

--- includes/frmTemoignage.php
+++ includes/frmTemoignage.php
@@ -2,7 +2,7 @@
     <ul>
         <li><label for="email">e-mail :</label><input type="text" id="email" name="email"  value="<?php echo $email;?>" /></li>
         <li><label for="dateTime">Date et heure de l'évènement :</label><input type="datetime-local" id="dateTime" name="dateTime" value="<?php echo $dateTime;?>"/></li>
-        <li><label for="duration">Durée de l'évènement :</label><input type="time" id="duration" name="duration" value="<?php echo $duration;?>"/></li>
+        <li><label for="duration">Durée de l'évènement (en minutes):</label><input type="time" id="duration" name="duration" value="<?php echo $duration;?>"/></li>
         <!-- <li><label for="location">Lieu de l'évènement :</label><select name="location"  id="location"></li> -->
         <li><label for="cardinalPoint">Direction de l'observation :</label><select name="cardinalPoint" id="cardinalPoint" value="<?php echo $cardinalPoint;?>">
             <option value="north">Nord</option>
