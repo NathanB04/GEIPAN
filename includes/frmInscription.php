728e06401adc7a28aa9fd2908723275845993dfe
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-03-02 11:40:35 +0100

    modif formulaire inscription

--- includes/frmInscription.php
+++ includes/frmInscription.php
@@ -3,7 +3,6 @@
         <li><label for="name">Nom :</label><input type="text" id="name" name="name" value="<?php echo $name;?>" /></li>
         <li><label for="firstname">Prénom :</label><input type="text" id="firstname" name="firstname"  value="<?php echo $firstname;?>" /></li>
         <li><label for="email">e-mail :</label><input type="text" id="email" name="email"  value="<?php echo $email;?>" /></li>
-        <li><label for="pseudo">Pseudo :</label><input type="text" id="pseudo" name="pseudo"  value="<?php echo $pseudo;?>" /></li>
         <li><label for="password">Mot de passe :</label><input type="password" id="password" name="password" /></li>
         <li><label for="passwordverif">Vérification mot de passe :</label><input type="password" id="passwordverif" name="passwordverif" /></li>
         <li><label for="avatar">Avatar :</label><input type="file" id="avatar" name="avatar" /></li>
