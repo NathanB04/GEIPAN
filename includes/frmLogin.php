55d3aa200e9b7d92928a56902721465bfb858dad
Nathan BOUQUET <nathanbouquet@protonmail.com>
2022-03-02 11:40:51 +0100

    creation formulaire login

---
+++ includes/frmLogin.php
@@ -0,0 +1,7 @@
+<form action="index.php?page=login" method="post">
+    <ul>
+        <li><label for="mail">e-mail :</label><input type="text" id="mail" name="mail" /></li>
+        <li><label for="mdp">Mot de passe</label><input type="password" id="mdp" name="mdp" /></li>
+        <li><input type="reset" value="Effacer" /><input type="submit" value="Envoyer" name="envoi" /></li>
+    </ul>
+</form>
